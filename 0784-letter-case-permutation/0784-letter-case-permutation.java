9a3e03e2c95b2e09dffc027beb3c5e39f11faf78
Tushar Saini <tushar.24scse1280045@galgotiasuniversity.ac.in>
2026-08-19 12:28:21 +0530

    Time: 5 ms (26.8%), Space: 53.3 MB (7.51%) - LeetHub

---
+++ 0784-letter-case-permutation/0784-letter-case-permutation.java
@@ -0,0 +1,33 @@
+class Solution {
+
+public void Solve ( List <String > ans , String curr , int i , String s){
+
+
+if ( i >= s.length()){
+    ans.add(curr);
+    return ;
+}
+  char ch = s.charAt(i);
+
+
+if (Character.isDigit(ch)){
+
+    Solve(ans , curr + Character.toLowerCase(ch) , i+1 ,s);
+    
+} 
+else{
+ Solve(ans , curr + Character.toLowerCase(ch) , i+1 ,s);
+  Solve(ans , curr + Character.toUpperCase(ch) , i+1 ,s);
+
+}
+}
+
+    public List<String> letterCasePermutation(String s) {
+        
+        List <String > ans = new ArrayList <>();
+
+        String curr = "";
+        Solve(ans , curr , 0  ,s);
+        return ans ;
+    }
+}
