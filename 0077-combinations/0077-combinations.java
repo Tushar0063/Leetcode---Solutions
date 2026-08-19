e0fc17a932183d9e0bd35401cfdb20725a08b103
Tushar Saini <tushar.24scse1280045@galgotiasuniversity.ac.in>
2026-08-20 00:26:31 +0530

    Time: 18 ms (79.81%), Space: 98 MB (46.57%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,19 +1,41 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
-        List<List<Integer>> result = new ArrayList<>();
-        generateCombinations(1, n, k, new ArrayList<Integer>(), result);
-        return result;
+        // List<List<Integer>> result = new ArrayList<>();
+        // generateCombinations(1, n, k, new ArrayList<Integer>(), result);
+        // return result;
+
+                
+                List<List<Integer>> ans = new ArrayList<>();
+                List<Integer> curr = new ArrayList<>();
+
+                generateCombinations ( 1 ,  n ,  k ,  curr, ans);
+
+                return ans ;
+
     }
 
-    private void generateCombinations(int start, int n, int k, List<Integer> combination, List<List<Integer>> result) {
-        if (k == 0) {
-            result.add(new ArrayList<>(combination));
-            return;
-        }
-        for (int i = start; i <= n - k + 1; i++) {
-            combination.add(i);
-            generateCombinations(i + 1, n, k - 1, combination, result);
-            combination.remove(combination.size() - 1);
-        }
+    public  void generateCombinations(int start, int n, int k, List<Integer> curr , List<List<Integer>> ans ) {
+        // if (k == 0) {
+        //     result.add(new ArrayList<>(combination));
+        //     return;
+        // }
+        // for (int i = start; i <= n - k + 1; i++) {
+        //     combination.add(i);
+        //     generateCombinations(i + 1, n, k - 1, combination, result);
+        //     combination.remove(combination.size() - 1);
+        // }
+
+
+         if(curr.size() == k){
+            ans.add(new ArrayList<>(curr));
+            return ;
+         }
+                
+                for(int i = start ; i<= n ;i++){
+                    curr.add(i);
+                     generateCombinations(i+1 ,n , k ,curr , ans );
+                     curr.remove(curr.size() - 1);
+                }
+
     }
 }
