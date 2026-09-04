65e0a641b765e10da61444f5ab8d21f5eb16f466
Tushar Saini <tushar.24scse1280045@galgotiasuniversity.ac.in>
2026-09-04 23:31:05 +0530

    Time: 1 ms (40.05%), Space: 45.1 MB (9.71%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,30 +1,45 @@
 class Solution {
 
-    public int Solve(int [] cost , int i , int [] dp){
+//     public int Solve(int [] cost , int i , int [] dp){
 
-        int l = cost.length;
-        if( i >= l) return 0 ;
+//         int l = cost.length;
+//         if( i >= l) return 0 ;
 
-if( dp[i] != -1 ) return dp[i];
+// if( dp[i] != -1 ) return dp[i];
 
-int oneStep = cost[i] + Solve(cost , i +1, dp);
+// int oneStep = cost[i] + Solve(cost , i +1, dp);
  
 
-int   twoStep = cost[i] +  Solve(cost  ,i+2 , dp);
+// int   twoStep = cost[i] +  Solve(cost  ,i+2 , dp);
 
- return dp[i] = Math.min(oneStep , twoStep);
+//  return dp[i] = Math.min(oneStep , twoStep);
 
+//     }
+//     public int minCostClimbingStairs(int[] cost) {
 
+//         int [] dp = new int[cost.length];
+//         Arrays.fill(dp , -1);
 
-
-    }
-    public int minCostClimbingStairs(int[] cost) {
-
-        int [] dp = new int[cost.length];
-        Arrays.fill(dp , -1);
-
-        return  Math.min(Solve(cost , 0 , dp) , Solve(cost , 1 , dp));
+//         return  Math.min(Solve(cost , 0 , dp) , Solve(cost , 1 , dp));
 
         
-    }
-}
+//     }
+
+      
+
+          public int minCostClimbingStairs(int[] cost) {
+
+            int n = cost.length;
+            int [] dp = new int [n+1];
+
+            dp[0] = 0;
+            dp[1]  = 0;
+
+            for(int i = 2 ; i<= n ; i++){
+                dp[i] = Math.min(dp[i-1] + cost[i-1] , dp[i-2] + cost[i-2]);
+            }
+ return dp[n];
+
+
+            
+}}
