d0ef4b972f0c5527dbe15a52ab11ed84f25da6f6
Tushar Saini <tushar.24scse1280045@galgotiasuniversity.ac.in>
2026-09-05 01:08:42 +0530

    Time: 0 ms (100%), Space: 42.6 MB (87.7%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,5 +1,7 @@
 class Solution {
 
+    // recursion 
+
     // public int Solve(int [] nums , int i ){
 
     //     int n = nums.length;
@@ -18,30 +20,52 @@ class Solution {
         
     // }
 
+    // memorisation 
 
-    public int Solve(int [] nums ,  int i , int [] dp){
+
+//     public int Solve(int [] nums ,  int i , int [] dp){
            
-           if ( i >= nums.length) return 0 ;
+//            if ( i >= nums.length) return 0 ;
 
-           if(dp[i] != -1) return dp[i];
+//            if(dp[i] != -1) return dp[i];
 
-           int one = nums[i] + Solve(nums , i+2 ,dp);
-           int two = Solve(nums , i+1 , dp);
+//            int one = nums[i] + Solve(nums , i+2 ,dp);
+//            int two = Solve(nums , i+1 , dp);
 
-           return dp[i] = Math.max(one , two);
+//            return dp[i] = Math.max(one , two);
+//     }
 
+//   public int rob(int[] nums) {
 
+//     int n = nums.length;
+//     int [] dp = new int[n+1];
 
-    }
+//     Arrays.fill(dp , -1);
 
-  public int rob(int[] nums) {
+//     return Solve(nums , 0 ,  dp);
+
+// }
+
+// Tabulation
+
+public int Solve(int [] nums ,  int [] dp){
 
     int n = nums.length;
-    int [] dp = new int[n+1];
+   dp[n] = 0; 
 
-    Arrays.fill(dp , -1);
+   for(int i = n-1 ; i>=0 ; i--){
 
-    return Solve(nums , 0 ,  dp);
-
+    dp[i] = Math.max( nums[i] + dp[i+2] , dp[i+1] );
+    
+   }
+return dp[0];
 }
-}
+
+ public int rob(int[] nums) {
+
+int dp[]  = new int [nums.length  + 2];
+
+return Solve(nums , dp);
+
+
+}}
