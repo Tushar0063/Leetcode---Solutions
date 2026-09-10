3873bcef651b40d02477b39768a640213939a3ee
Tushar Saini <tushar.24scse1280045@galgotiasuniversity.ac.in>
2026-09-10 11:20:05 +0530

    Time: 1 ms (81.28%), Space: 42.9 MB (76.96%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -24,34 +24,59 @@ class Solution {
 
 // MEMORISATION 
 
-public int Solve(int i , String s ,int [] dp){
+// public int Solve(int i , String s ,int [] dp){
 
-    if( i >= s.length()) return 1;
-    if (s.charAt(i) == '0') return 0;
-    if(dp[i] != -1) return dp[i];
+//     if( i >= s.length()) return 1;
+//     if (s.charAt(i) == '0') return 0;
+//     if(dp[i] != -1) return dp[i];
 
-    int single = Solve(i+1 , s ,dp);
+//     int single = Solve(i+1 , s ,dp);
 
-    int two = 0 ;
-    if(i < s.length()-1) {
-        int curr = s.charAt(i) - 48;
-        int next = s.charAt(i+1) - 48;
+//     int two = 0 ;
+//     if(i < s.length()-1) {
+//         int curr = s.charAt(i) - 48;
+//         int next = s.charAt(i+1) - 48;
 
-        int num = curr*10 + next;
-        if (num <= 26) two = Solve(i+2 , s, dp);
+//         int num = curr*10 + next;
+//         if (num <= 26) two = Solve(i+2 , s, dp);
 
-    }
-    dp[i] = single + two;
-    return dp[i];
-}
+//     }
+//     dp[i] = single + two;
+//     return dp[i];
+// }
 
     public int numDecodings(String s) {
         
       // return Solve(s,0);
 
-      int [] dp = new int [s.length()+ 1];
-       Arrays.fill(dp,-1);
-       return Solve(0 , s, dp);
+    //   int [] dp = new int [s.length()+ 1];
+    //    Arrays.fill(dp,-1);
+    //    return Solve(0 , s, dp);
+
+    // TABULATION 
+    int n = s.length();
+    int [] dp = new int[n+1];
+
+    dp[n] = 1;
+
+    for(int i = n-1 ; i>= 0 ;i--){
+       if (s.charAt(i) == '0') continue ;
+
+       int single = dp[i+1];
+       int two = 0 ;
+
+       if (i < n-1)  {
+        int curr = s.charAt(i) - 48;
+        int next = s.charAt(i+1) - 48;
+
+         int num = curr*10 + next;
+         if(num <= 26) two = dp[i+2];
+       }
+
+       dp[i] = single + two;
+
+    }
+     return dp[0];
 
     }
 }
