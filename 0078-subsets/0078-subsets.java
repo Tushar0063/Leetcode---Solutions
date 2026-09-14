07a2480695c2a6d9ac73db6c4876db1a29c827c1
Tushar Saini <tushar.24scse1280045@galgotiasuniversity.ac.in>
2026-09-14 12:56:38 +0530

    Time: 1 ms (87.18%), Space: 43.8 MB (93.97%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,36 +1,35 @@
 class Solution {
 
-public void Solve(int [] nums , int i , List<List<Integer>> ans ,  List<Integer> curr   ){
+public void Solve(int [] nums , int i ,  List<List<Integer>> ans,   List<Integer> curr){
 
-    if(i >= nums.length){
-
+    int n = nums.length;
+    if (i >= n){
         ans.add(new ArrayList<>(curr));
-        return ; 
+        return ;
     }
 
- int temp =  nums[i];
+    int temp = nums[i];
+    curr.add(temp);
 
+    // take 
+    Solve(nums , i+1 , ans ,  curr);
 
- curr.add(temp);
- // take 
- Solve(nums , i+1 , ans , curr);
-
- // skip 
- curr.remove(curr.size()- 1);
- Solve(nums , i+1 , ans ,curr);
-
+    // skip 
+    curr.remove(curr.size() - 1);
+    Solve(nums , i+1 , ans , curr);
 
 
 }
 
-
     public List<List<Integer>> subsets(int[] nums) {
 
-    List<List<Integer>> ans = new ArrayList<>();
-    List<Integer> curr = new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> curr = new ArrayList<>();
+           
 
-    Solve(nums, 0 ,ans , curr);
-    return ans ; 
+           Solve(nums , 0 ,  ans ,  curr);
+           return ans;
 
+        
     }
 }
