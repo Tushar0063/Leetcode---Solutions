b792670092c58820418633af470fd1b4c1a95e6d
Tushar Saini <tushar.24scse1280045@galgotiasuniversity.ac.in>
2026-09-14 23:41:28 +0530

    Time: 6 ms (16.48%), Space: 53.2 MB (8.14%) - LeetHub

--- 0784-letter-case-permutation/0784-letter-case-permutation.java
+++ 0784-letter-case-permutation/0784-letter-case-permutation.java
@@ -1,33 +1,32 @@
 class Solution {
 
-public void Solve ( List <String > ans , String curr , int i , String s){
+    public void Solve(String s ,  List<String> ans , String curr , int i ){
 
+        
+    int n = s.length();
+    if (i == n){
+        ans.add(curr);
+        return ;
+    }
 
-if ( i >= s.length()){
-    ans.add(curr);
-    return ;
-}
-  char ch = s.charAt(i);
+          char ch = s.charAt(i);
+          if (Character.isDigit(ch)){
+            Solve(s,ans,curr+ch,i+1);
+          } else{
+          Solve(s, ans , curr + Character.toUpperCase(ch) , i+1);
+          Solve(s, ans ,  curr + Character.toLowerCase(ch) , i+1);
+          }
 
-
-if (Character.isDigit(ch)){
-
-    Solve(ans , curr + ch , i+1 ,s);
-
-} 
-else{
- Solve(ans , curr + Character.toLowerCase(ch) , i+1 ,s);
-  Solve(ans , curr + Character.toUpperCase(ch) , i+1 ,s);
-
-}
-}
-
+    }
     public List<String> letterCasePermutation(String s) {
         
-        List <String > ans = new ArrayList <>();
 
+        List<String> ans = new ArrayList<>();
         String curr = "";
-        Solve(ans , curr , 0  ,s);
+
+        Solve(s,ans ,curr , 0);
+
         return ans ;
+
     }
 }
