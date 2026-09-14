87b97a419358d56b4557f6e1fcc0743dccf1aa25
Tushar Saini <tushar.24scse1280045@galgotiasuniversity.ac.in>
2026-09-14 18:16:37 +0530

    Time: 2 ms (99.73%), Space: 44.9 MB (88.62%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,35 +1,40 @@
 class Solution {
 
-public void Solve( List<List<Integer>> ans ,  List<Integer > curr , int [] nums , int i){
+    public static void Solve(int [] nums , int i , List<List<Integer>> ans , List<Integer>curr ){
 
-    if(i == nums.length) {
+       int n = nums.length;
+       if ( i >= n){
         ans.add(new ArrayList<>(curr));
-        return ;
+        return;
+       }
+
+       int temp = nums[i];
+       curr.add(temp);
+        
+        // Take 
+        Solve(nums , i+1 , ans , curr);
+
+        // Skip
+        curr.remove(curr.size() - 1);
+
+        // for duplicate handle
+  while ( i+1 < nums.length && nums[i] == nums[i+1]){
+    i++;
+  }
+  // skip 
+  Solve(nums , i+1 , ans ,curr);
+
+       
+
     }
-
-    // take
-    int temp = nums[i];
-    curr.add(temp);
-    Solve(ans , curr , nums , i+1);
-
-    // skip 
-    curr.remove(curr.size() - 1);
-
-    while (i + 1 < nums.length && nums[i] == nums[i + 1]) {
-            i++;
-        }
-    Solve(ans , curr , nums , i+1);
-
-}
-
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         
         List<List<Integer>> ans = new ArrayList<>();
-        List<Integer > curr = new ArrayList<>();
+        List<Integer> curr = new ArrayList<>();
 
-               Arrays.sort(nums);
-      Solve(ans , curr , nums , 0);
-      return ans ;
-
+        Arrays.sort(nums );
+       
+       Solve(nums , 0 , ans ,curr);
+       return ans ;
     }
 }
