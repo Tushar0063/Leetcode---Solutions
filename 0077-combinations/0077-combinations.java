9d5aca345e852ab05af88668792ac26dc4db1bb7
Tushar Saini <tushar.24scse1280045@galgotiasuniversity.ac.in>
2026-09-14 22:08:25 +0530

    Time: 22 ms (18.8%), Space: 98.1 MB (46.53%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,41 +1,38 @@
 class Solution {
-    public List<List<Integer>> combine(int n, int k) {
-        // List<List<Integer>> result = new ArrayList<>();
-        // generateCombinations(1, n, k, new ArrayList<Integer>(), result);
-        // return result;
 
-                
-                List<List<Integer>> ans = new ArrayList<>();
-                List<Integer> curr = new ArrayList<>();
+    public void Solve(int nums[] , int i , int k ,    List<List<Integer>> ans,  List<Integer> curr){
 
-                generateCombinations ( 1 ,  n ,  k ,  curr, ans);
+       
+  
+  if ( curr.size() == k){
+    ans.add(new ArrayList<>(curr));
+    return ;
+  }
+   if ( i >= nums.length) return;
+     
+     
+      curr.add(nums[i]);
+     
+      // take 
+      Solve(nums , i+1 , k , ans , curr);
 
-                return ans ;
+      // skip 
+      curr.remove(curr.size()-1);
+      Solve(nums , i+1 , k , ans ,curr);
+
+
 
     }
+    public List<List<Integer>> combine(int n, int k) {
+        
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> curr = new ArrayList<>();
 
-    public  void generateCombinations(int start, int n, int k, List<Integer> curr , List<List<Integer>> ans ) {
-        // if (k == 0) {
-        //     result.add(new ArrayList<>(combination));
-        //     return;
-        // }
-        // for (int i = start; i <= n - k + 1; i++) {
-        //     combination.add(i);
-        //     generateCombinations(i + 1, n, k - 1, combination, result);
-        //     combination.remove(combination.size() - 1);
-        // }
-
-
-         if(curr.size() == k){
-            ans.add(new ArrayList<>(curr));
-            return ;
-         }
-                
-                for(int i = start ; i<= n ;i++){
-                    curr.add(i);
-                     generateCombinations(i+1 ,n , k ,curr , ans );
-                     curr.remove(curr.size() - 1);
-                }
-
+        int [] nums = new int [n];
+        for (int i = 0 ; i< n ; i++){
+            nums[i] = i+1;
+        }
+  Solve(nums , 0 , k , ans ,curr);
+  return ans ;
     }
 }
