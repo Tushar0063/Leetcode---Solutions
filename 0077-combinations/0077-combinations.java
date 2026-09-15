043a8dc9e8dea975ccabe991bae91f5ea54b3dca
Tushar Saini <tushar.24scse1280045@galgotiasuniversity.ac.in>
2026-09-16 00:39:44 +0530

    Time: 19 ms (49.51%), Space: 98.1 MB (28.11%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,6 +1,6 @@
 class Solution {
 
-    public void Solve(int nums[] , int i , int k ,    List<List<Integer>> ans,  List<Integer> curr){
+    public void Solve( int start  ,int n , int k ,    List<List<Integer>> ans,  List<Integer> curr){
 
        
   
@@ -8,31 +8,41 @@ class Solution {
     ans.add(new ArrayList<>(curr));
     return ;
   }
-   if ( i >= nums.length) return;
+   
      
-     
-      curr.add(nums[i]);
-     
-      // take 
-      Solve(nums , i+1 , k , ans , curr);
+     for (int i = start ; i <=  n; i++){
+    curr.add(i);
+    Solve(i+1 ,n , k , ans , curr);
 
-      // skip 
-      curr.remove(curr.size()-1);
-      Solve(nums , i+1 , k , ans ,curr);
+    curr.remove(curr.size() - 1);
 
-
-
+     }
     }
+     
+    //   curr.add(nums[i]);
+     
+    //   // take 
+    //   Solve(nums , i+1 , k , ans , curr);
+
+    //   // skip 
+    //   curr.remove(curr.size()-1);
+    //   Solve(nums , i+1 , k , ans ,curr);
+
+
+
+    
     public List<List<Integer>> combine(int n, int k) {
         
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> curr = new ArrayList<>();
 
-        int [] nums = new int [n];
-        for (int i = 0 ; i< n ; i++){
-            nums[i] = i+1;
-        }
-  Solve(nums , 0 , k , ans ,curr);
+//         int [] nums = new int [n];
+//         for (int i = 0 ; i< n ; i++){
+//             nums[i] = i+1;
+//         }
+//   Solve(nums , 0 , k , ans ,curr);
+
+Solve(1 , n , k ,ans ,curr);
   return ans ;
     }
-}
+    }
