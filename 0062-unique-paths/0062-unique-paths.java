927f69fc0b5cac683345b9724b83f36b56a6dfc0
Tushar Saini <tushar.24scse1280045@galgotiasuniversity.ac.in>
2026-09-17 01:02:20 +0530

    Time: 0 ms (100%), Space: 42.3 MB (28.66%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,37 +1,50 @@
 class Solution {
-    public int Solve(int m , int n , int r , int c , int [][] dp){
+    // public int Solve(int m , int n , int r , int c , int [][] dp){
 
-
-        // if(dp[r][c] != -1) return dp[r][c];
-
-
-
-          if (r == m-1 && c == n-1) return 1 ;
-        if ( r > m || c> n) return 0;
+    //       if (r == m-1 && c == n-1) return 1 ;
+    //     if ( r > m || c> n) return 0;
       
 
-     if(dp[r][c] != -1) return dp[r][c];
+    //  if(dp[r][c] != -1) return dp[r][c];
 
-        int down = Solve(m , n ,r+1 , c , dp);
-        int right = Solve(m , n , r , c+1 , dp);
+    //     int down = Solve(m , n ,r+1 , c , dp);
+    //     int right = Solve(m , n , r , c+1 , dp);
 
-        dp[r][c] = (down + right);
+    //     dp[r][c] = (down + right);
 
-        return dp[r][c] ;
-    }
+    //     return dp[r][c] ;
+    // }
     public int uniquePaths(int m, int n) {
 
        // return Solve( m ,n , 0 , 0);
 
        int [][] dp = new int [m+1][n+1];
     
-      for(int i = 0 ; i<= m ; i++){
-        for(int j = 0 ; j <= n ;j++){
-            dp[i][j] = -1;
-        }
-      }
+    //   for(int i = 0 ; i<= m ; i++){
+    //     for(int j = 0 ; j <= n ;j++){
+    //         dp[i][j] = -1;
+    //     }
+    //   }
 
-       return Solve(m , n ,0 , 0 , dp);
+    //    return Solve(m , n ,0 , 0 , dp);
 
+    for(int i = 0 ; i<m ; i++){
+        dp[i][n-1] = 1;
     }
+    for (int j = 0 ; j < n ;j++){
+        dp[m-1][j] = 1;
+    }
+
+    for(int i = m-2 ; i >= 0 ;i--){
+    for(int j = n-2 ; j >= 0 ; j--){
+
+           int down = dp[i+1][j];
+           int right = dp[i][j+1];
+
+           dp[i][j] = down + right ;
 }
+          }
+    
+    return dp[0][0];
+    }
+    }
