afa2265da6f6af851bb56376d68ccdca2c40d966
Tushar Saini <tushar.24scse1280045@galgotiasuniversity.ac.in>
2026-09-17 10:00:00 +0530

    Time: 0 ms (100%), Space: 43.2 MB (91.19%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,39 +1,70 @@
 class Solution {
 
-    public int Solve(int [][] obstacleGrid , int r , int c , int [][] dp){
+    // public int Solve(int [][] obstacleGrid , int r , int c , int [][] dp){
          
        
-        int m = obstacleGrid.length;
-        int n = obstacleGrid[0].length;
+    //     int m = obstacleGrid.length;
+    //     int n = obstacleGrid[0].length;
 
-        if (r >= m || c >= n) return 0;
-         if (obstacleGrid[r][c] == 1 ) return 0 ; 
-          if (r == m-1 && c == n-1) return 1 ;
+    //     if (r >= m || c >= n) return 0;
+    //      if (obstacleGrid[r][c] == 1 ) return 0 ; 
+    //       if (r == m-1 && c == n-1) return 1 ;
 
-          if(dp[r][c] != -1) return dp[r][c];
+    //       if(dp[r][c] != -1) return dp[r][c];
 
-       int down  = Solve(obstacleGrid , r+1 , c ,dp);
-       int right = Solve(obstacleGrid , r, c+1 ,dp);
+    //    int down  = Solve(obstacleGrid , r+1 , c ,dp);
+    //    int right = Solve(obstacleGrid , r, c+1 ,dp);
 
-       dp[r][c] = down + right ;
-       return dp[r][c];
+    //    dp[r][c] = down + right ;
+    //    return dp[r][c];
 
-    }
+    // }
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         
       
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
 
-          int [][] dp = new int [m+1][n+1];
-          for (int i = 0 ; i<= m ;i++){
-            for(int j = 0 ; j<= n ;j++){
-                dp[i][j] = -1;
-            }
-          }
+//           int [][] dp = new int [m+1][n+1];
+//           for (int i = 0 ; i<= m ;i++){
+//             for(int j = 0 ; j<= n ;j++){
+//                 dp[i][j] = -1;
+//             }
+//           }
            
-return Solve(obstacleGrid , 0 , 0 , dp);
+// return Solve(obstacleGrid , 0 , 0 , dp);
 
         // return Solve(obstacleGrid , 0 , 0);
-    }
-}
+
+if (obstacleGrid[m - 1][n - 1] == 1 || obstacleGrid[0][0] == 1) {
+            return 0;
+        }
+
+        int [][] dp = new int[m+1][n+1];
+
+        dp[m-1][n-1]  = 1;
+        
+
+        for (int i = m-1 ; i >= 0 ; i--){
+            for(int j = n-1 ; j >= 0 ; j--){
+        if (i == m - 1 && j == n - 1) {
+                    continue;
+        }
+    
+          if(obstacleGrid[i][j] == 1) {
+            dp[i][j] =  0 ;
+          } 
+          else {
+
+       int   down = dp[i+1][j];
+       int   right = dp[i][j+1];
+
+          dp[i][j] = down + right;
+            }
+            }
+        }
+        
+        return dp[0][0];
+
+
+}}
